0eadc089f47b82d22a3e0d5cb99ef35a1a744da1
Karim Fraboulet <karim.fraboulet@gmail.com>
2022-12-28 14:02:21 +0100

    Initial Commit

---
+++ AdaSurf/AdaSurfApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdaSurfApp.swift
+//  AdaSurf
+//
+//  Created by Karim Fraboulet on 28/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct AdaSurfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
