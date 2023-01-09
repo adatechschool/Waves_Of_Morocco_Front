c70024af2327ee4e28449b8778c2ffe549a7f106
Karim Fraboulet <karim.fraboulet@gmail.com>
2023-01-09 15:16:05 +0100

    same as Delhia

--- AdaSurf/View/LandmarkRow.swift
+++ AdaSurf/View/LandmarkRow.swift
@@ -1,33 +1,25 @@
 //
-//  LandmarkRow.swift
-//  AdaSurf
+// LandmarkRow.swift
+// AdaSurf
 //
-//  Created by Karim Fraboulet on 04/01/2023.
+// Created by Karim Fraboulet on 04/01/2023.
 //
-
 import SwiftUI
 
 struct LandmarkRow: View {
-    let landmark: LandmarkListElement
-
-    var body: some View {
-        HStack{
-            landmark.image
-                .resizable()
-                .frame(width: 50, height: 50)
-            Text("\(landmark.name)")
-            
-            Spacer()
-        }
+  var landmark: Landmark
+  var body: some View {
+    HStack {
+      landmark.image
+        .resizable()
+        .frame(width: 50, height: 50)
+      Text(landmark.name)
+      Spacer()
     }
+  }
 }
-
 struct LandmarkRow_Previews: PreviewProvider {
-    static let items: LandmarkList = FileLoader.load("landmarkData.json")
-    static var previews: some View {
-        Group{
-            LandmarkRow(landmark: LandmarkRow_Previews.items[0])
-        }
-        .previewLayout(.fixed(width: 300, height: 70))
-    }
+  static var previews: some View {
+    LandmarkRow(landmark: landmarks[1])
+  }
 }
